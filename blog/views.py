1a9a3e4ed48261115407bd13713643d40275f495
gogen02 <superobzorchikbomg3000@gmail.com>
2025-02-07 09:08:01 -0500

    Initial commit

---
+++ blog/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+from .models import Post
+from django.views.generic import ListView, DetailView
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class BlogDetailView(DetailView): # новое
+    model = Post
+    template_name = 'post_detail.html'
